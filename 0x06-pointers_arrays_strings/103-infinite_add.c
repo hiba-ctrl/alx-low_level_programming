604647a65ed9c032a40d57530d0e5f8b5c33d7a8
hiba-ctrl <hibaelouariti@gmail.com>
2023-10-19 17:13:27 -0700

    arrays

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -24,32 +24,6 @@ void rev_string(char *n)
 		*(n + j) = *(n + i);
 		*(n + i) = temp;
 	}
-#include "main.h"
-
-/**
- * rev_string - reverse array
- * @n: integer params
- * Return: 0
- */
-
-void rev_string(char *n)
-{
-	int i = 0;
-	int j = 0;
-	char temp;
-
-	while (*(n + i) != '\0')
-	{
-		i++;
-	}
-	i--;
-
-	for (j = 0; j < i; j++, i--)
-	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
-	}
 }
 
 /**
