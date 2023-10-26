ee9fc463bfe3eb861d126c081ab9ec0cc1e9cb2d
hiba-ctrl <hibaelouariti@gmail.com>
2023-10-26 10:12:56 -0700

    Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,19 @@
 #include "main.h"
+
 /**
- * _puts_recursion - function like puts()
- * @s : input
- * Return: 0 
- */
-
-void _puts_recursion(char *s){
-
-	if(*s){
-		_putchar(*s);
-		_puts_recursion(s+1);
+  * _puts_recursion - main function
+  *
+  * @s: function parameter
+  *
+  * Return: Always 0.
+  */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+	_putchar(*s);
+	_puts_recursion(s + 1);
 	}
 	else
-		_putchar('\n'); }
+	_putchar('\n');
+}
