6bc70e1e8d5dd396faa1e2be4240667dd4068a9d
hiba-ctrl <hibaelouariti@gmail.com>
2023-10-27 13:34:17 -0700

    Static library

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - check uppercase letters
+ * @c: char to check
+ *
+ * Return: if uppercase return 1, else 0
+ */
+int _isupper(int c)
+{
+	if (c>='A' && c <= 'Z')
+		return(1);
+
+        return (0);
+}
---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_isdigit - checks for digit
+ *@c: int checked
+ *Return 1 if c is a digit else 0
+ */
+int _isdigit(int c)
+{
+	return(c>='0' && c<='9');
+}
