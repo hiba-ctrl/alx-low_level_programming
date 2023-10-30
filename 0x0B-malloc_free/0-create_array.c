bbe82956d027a11f37210d96477b4fef6b32ff1c
hiba-ctrl <hibaelouariti@gmail.com>
2023-10-30 08:11:59 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,20 +8,24 @@
  *
  * Return: a pointer to char
  */
-char *create_array(unsigned int size, char c) {
+char *create_array(unsigned int size, char c)
+{
     char *ar;
     unsigned int i;
 
-    if (size == 0) {
-        return NULL;
+    if (size == 0)
+    {
+        return(NULL);
     }
     
     ar = malloc(size * sizeof(char));
-    if (ar == NULL) {
-        return NULL;
+    if (ar == NULL)
+    {
+        return(NULL);
     }
 
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < size; i++)
+    {
         ar[i] = c;
     }
 
