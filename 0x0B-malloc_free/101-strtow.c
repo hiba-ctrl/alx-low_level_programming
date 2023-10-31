01d46de25fa59930f1c190f51f81e7f2ef0b202b
hiba-ctrl <hibaelouariti@gmail.com>
2023-10-31 12:23:53 -0700

    malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -6,52 +6,41 @@
  * @str: The string to be split.
  *
  * Return: If str == NULL, str == "", or the function fails - NULL.
- *         Otherwise - a pointer to an array of strings (words).
+ *         Otherwise - a pointer to an array of strings
  */
 char **strtow(char *str)
 {
-	char **words;
-	int i = 0, j, k, l, count = 0, len = 0;
+    char **words;
+    int i = 0, j, w = 0, wc = 0, len;
 
-	if (!str || !*str)
-		return (NULL);
+    for (i = 0; str[i]; i++)
+        wc += (str[i] != ' ' && (!i || str[i - 1] == ' ')) ? 1 : 0;
 
-	/* Count words */
-	for (i = 0; str[i]; i++)
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-			count++;
+    if (!str || !*str || !wc)
+        return (NULL);
 
-	if (count == 0)
-		return (NULL);
+    words = malloc((wc + 1) * sizeof(char *));
+    if (!words)
+        return (NULL);
 
-	words = malloc((count + 1) * sizeof(char *));
-	if (!words)
-		return (NULL);
+    for (i = 0, j = 0; j < wc; j++, w = 0)
+    {
+        while (str[i] == ' ')
+            i++;
+        len = i;
+        while (str[len] && str[len++] != ' ');
 
-	for (i = 0, j = 0; j < count; j++)
-	{
-		while (str[i] == ' ')
-			i++;
+        words[j] = malloc(len - i + 1);
+        if (!words[j])
+            for (; j >= 0; j--)
+                free(words[j]), free(words), words = NULL;
+        if (!words)
+            return (NULL);
 
-		l = i;
-		while (str[i] && str[i++] != ' ')
-			len++;
-
-		words[j] = malloc((len + 1) * sizeof(char));
-		if (!words[j])
-		{
-			for (k = 0; k < j; k++)
-				free(words[k]);
-			free(words);
-			return (NULL);
-		}
-
-		for (k = 0; l < i - 1; l++, k++)
-			words[j][k] = str[l];
-		words[j][k] = '\0';
-		len = 0;
-	}
-	words[j] = NULL;
-
-	return (words);
+        while (str[i] && str[i] != ' ')
+            words[j][w++] = str[i++];
+        words[j][w] = '\0';
+    }
+    words[j] = NULL;
+    return (words);
 }
