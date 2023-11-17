d94165b029513d068b82e87ebc5a7e12998d4b0f
hiba-ctrl <hibaelouariti@gmail.com>
2023-11-17 09:45:57 -0800

    Linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,44 @@
 #include "lists.h"
-#include <stddef.h>
 
 /**
  * _putchar - Writes the character c to stdout.
  * @c: The character to print.
- * Return: On success 1. On error, -1 is returned, and errno is set appropriately.
+ * Return: On success 1, on error -1, and errno is set appropriately.
  */
 int _putchar(char c);
 
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
+ * print_number - Prints an unsigned number.
+ * @n: The number to be printed.
+ */
+void print_number(unsigned int n)
+{
+    if (n / 10)
+        print_number(n / 10);
+    _putchar((n % 10) + '0');
+}
+
+/**
+ * print_list - Prints all elements of a list_t list.
+ * @h: Pointer to the start of the list.
  * Return: The number of nodes.
  */
 size_t print_list(const list_t *h)
 {
     size_t nodes = 0;
-    int i;
 
     while (h)
     {
-        if (h->str == NULL)
+        _putchar('[');
+        if (h->str)
         {
-            _putchar('[');
+            print_number(h->len);
+            _putchar(']');
+            _putchar(' ');
+            _putchar(h->str);
+        }
+        else
+        {
             _putchar('0');
             _putchar(']');
             _putchar(' ');
@@ -32,17 +48,9 @@ size_t print_list(const list_t *h)
             _putchar('l');
             _putchar(')');
         }
-        else
-        {
-            for (i = 0; h->str[i]; i++)
-            {
-                _putchar(h->str[i]);
-            }
-        }
         _putchar('\n');
         nodes++;
         h = h->next;
     }
-
     return nodes;
 }
