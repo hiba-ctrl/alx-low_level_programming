9a87bea41954efbbea96b6ae97e55e5ca86d5ebe
hiba-ctrl <hibaelouariti@gmail.com>
2023-11-17 09:38:17 -0800

    Linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,9 @@
-#ifndef LISTS_H  // Corrected the header guard
+#ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>  // Include for size_t
+#include <stddef.h>  /* Include for size_t */
 
-// Assuming list_t is defined elsewhere or included from another file
-typedef struct list_s list_t;
+/* Structure for single linked list nodes */
 typedef struct list_s
 {
     char *str;
@@ -12,11 +11,11 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
+/* Function prototypes */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif // LISTS_H
-
+#endif /* LISTS_H */
