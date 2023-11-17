ad7bad5d2582a796263a02411e6e20ea080d1a52
hiba-ctrl <hibaelouariti@gmail.com>
2023-11-17 09:31:39 -0800

    Linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,37 +2,21 @@
 #include <stddef.h>
 
 /**
- * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
- *
- * Return: The length of @str.
- */
-static int _strlen(const char *str)
-{
-    int len = 0;
-    while (str && str[len])
-        len++;
-    return len;
-}
-
-/**
  * _putchar - Writes the character c to stdout.
  * @c: The character to print.
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: On success 1. On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
 
 /**
  * print_list - Prints all the elements of a list_t list.
  * @h: The list_t list.
- *
  * Return: The number of nodes.
  */
 size_t print_list(const list_t *h)
 {
     size_t nodes = 0;
+    int i;
 
     while (h)
     {
@@ -50,12 +34,11 @@ size_t print_list(const list_t *h)
         }
         else
         {
-            for (int i = 0; i < _strlen(h->str); i++)
+            for (i = 0; h->str[i]; i++)
             {
                 _putchar(h->str[i]);
             }
         }
-
         _putchar('\n');
         nodes++;
         h = h->next;
@@ -63,4 +46,3 @@ size_t print_list(const list_t *h)
 
     return nodes;
 }
-
